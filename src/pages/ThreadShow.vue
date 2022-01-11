0ababa4380c26a8b30f6d74bf92cb181f4dd554e
Danila Prokoshev <danilaprokoshev@gmail.com>
2022-01-11 15:40:30 +0300

    add new post handling

--- src/pages/ThreadShow.vue
+++ src/pages/ThreadShow.vue
@@ -3,7 +3,22 @@
     <h1>{{ thread.title }}</h1>
 
     <post-list :posts="threadPosts" />
+    <form @submit.prevent="addPost">
+      <div class="form-group">
+        <label for="thread_title">Title:</label>
+        <input type="text" id="thread_title" class="form-input" name="title" >
+      </div>
 
+      <div class="form-group">
+        <label for="thread_content">Content:</label>
+        <textarea id="thread_content" v-model="newPostText" class="form-input" name="content" rows="8" cols="140"></textarea>
+      </div>
+
+      <div class="btn-group">
+        <button class="btn btn-ghost">Cancel</button>
+        <button class="btn btn-blue" type="submit" name="Publish">Publish </button>
+      </div>
+    </form>
   </div>
   <div v-else>
     <h1>Not Found</h1>
@@ -27,7 +42,8 @@ export default {
   data () {
     return {
       threads: sourceData.threads,
-      posts: sourceData.posts
+      posts: sourceData.posts,
+      newPostText: ''
     }
   },
   computed: {
@@ -36,6 +52,22 @@ export default {
     },
     threadPosts () {
       return this.posts.filter(post => post.threadId === this.id)
+    }
+  },
+  methods: {
+    addPost () {
+      const postId = 'qqq' + Math.random()
+      const post = {
+        id: postId,
+        text: this.newPostText,
+        publishedAt: Math.floor(Date.now() / 1000),
+        threadId: this.id,
+        userId: '38St7Q8Zi2N1SPa5ahzssq9kbyp1'
+      }
+      this.posts.push(post)
+      this.thread.posts.push(postId)
+
+      this.newPostText = ''
     }
   }
 }
