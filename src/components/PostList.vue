79d99814cb774b5a59e2cb5c73d997d75ba284b1
Danila Prokoshev <danilaprokoshev@gmail.com>
2022-01-21 09:46:19 +0300

    add base component for readdable dates

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -17,8 +17,8 @@
           </p>
         </div>
       </div>
-      <div class="post-date text-faded" :title="humanFriendlyDate(post.publishedAt)">
-        {{ diffForHumans(post.publishedAt) }}
+      <div class="post-date text-faded">
+        <AppDate :timestamp="post.publishedAt" />
       </div>
     </div>
   </div>
@@ -26,11 +26,7 @@
 
 <script>
 import sourceData from '@/data.json'
-import dayjs from 'dayjs'
-import relativeTime from 'dayjs/plugin/relativeTime'
-import localizedDate from 'dayjs/plugin/localizedFormat'
-dayjs.extend(relativeTime)
-dayjs.extend(localizedDate)
+import AppDate from '@/components/AppDate'
 
 export default {
   name: 'PostList',
@@ -40,6 +36,9 @@ export default {
       required: true
     }
   },
+  components: {
+    AppDate
+  },
   data () {
     return {
       users: sourceData.users
@@ -48,12 +47,6 @@ export default {
   methods: {
     userById (userId) {
       return this.users.find(u => u.id === userId)
-    },
-    diffForHumans (timestamp) {
-      return dayjs.unix(timestamp).fromNow()
-    },
-    humanFriendlyDate (timestamp) {
-      return dayjs.unix(timestamp).format('llll')
     }
   }
 }
