e28bd388a459c238da86cdca5bcd27fe0d9125bb
Danila Prokoshev <danilaprokoshev@gmail.com>
2022-02-05 15:08:02 +0300

    apdate user profile

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -3,6 +3,7 @@
     <div class="flex-grid">
       <div class="col-3 push-top">
         <UserProfileCard :user="user" />
+        <UserProfileCardEditor :user="user" />
         <p class="text-xsmall text-faded text-center">Member since june 2003, last visited 4 hours ago</p>
 
         <div class="text-center">
@@ -32,11 +33,12 @@
 <script>
 import PostList from '@/components/PostList'
 import UserProfileCard from '@/components/UserProfileCard'
+import UserProfileCardEditor from '@/components/UserProfileCardEditor'
 import { mapGetters } from 'vuex'
 
 export default {
   name: 'Profile',
-  components: { PostList, UserProfileCard },
+  components: { PostList, UserProfileCard, UserProfileCardEditor },
   computed: {
     ...mapGetters({ user: 'authUser' })
   }
